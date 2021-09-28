c8063964bef8fdb04f6ab0f8ffe9dc1bce542a46
Sebastian Szocs <6sego1@gmail.com>
2021-09-28 11:29:27 +0200

    Initial Commit

---
+++ CTFeedbackSwiftLanguages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CTFeedbackSwiftLanguages
+//
+//  Created by Sebastian Szöcs on 9/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
